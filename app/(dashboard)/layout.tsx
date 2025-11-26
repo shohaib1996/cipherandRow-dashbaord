2cd270a4c05e3b44c29389ab18671d23879a321a
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-11-27 00:37:06 +0600

    setup done

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers";
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value !== "false";
+
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <div className="h-4 w-px bg-zinc-200 dark:bg-zinc-800" />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>THis is dashboard</h1>;
+}
